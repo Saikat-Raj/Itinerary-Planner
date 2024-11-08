408fe44ec64a268af5d77da92c95e1595d6592a1
Saikat-Raj <saikat.raj.cs@gmail.com>
2024-11-07 23:33:00 -0400

    Configuring MySQL database settings for Django project

--- itinerary_planner/settings.py
+++ itinerary_planner/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "itinerary_planner.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "itinerary_planner",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
